6feeea88d695f4e1eba241fca6e94dde18e70ba2
howdy-mj <hi.minjungkim@gmail.com>
2020-03-03 14:31:00 +0900

    2.0 Add a Clock

---
+++ review/clock.js
@@ -0,0 +1,23 @@
+const timeContainer = document.querySelector(".time");
+const timeTxt = timeContainer.querySelector("h3");
+
+function time() {
+    const clock = new Date();
+    const year = clock.getFullYear();
+    const month = clock.getMonth();
+    const date = clock.getDate();
+    const hour = clock.getHours();
+    const min = clock.getMinutes();
+    const second = clock.getSeconds();
+
+    timeTxt.innerText = `${year}/${month}/${date} ${
+                                                    hour < 10 ? `0${hour}` : hour}:${
+                                                    min < 10 ? `0${min}` : min}:${
+                                                    second < 10 ? `0${second}` : second}`;
+}
+
+function init() {
+    time();
+    setInterval(time, 1000);
+}
+init();
