721c78959dfa4cc7eef47228f43dd250a7bf7e5d
howdy-mj <hi.minjungkim@gmail.com>
2020-03-04 17:25:43 +0900

    4.2 Add load, submit, write, save ToDo

--- review/todo.js
+++ review/todo.js
@@ -2,16 +2,53 @@ const todoForm = document.querySelector(".todoForm");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector(".todoList");
 
-const toDos = [];
+const TODOS_LS = "toDos";
+let toDos = [];
+
+function saveToDo() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function writeToDo(text) {
+    // console.log(localStorage.getItem(TODOS_LS));
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+
+    delBtn.innerText = "삭제";
+    span.innerText = text;
+
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    todoList.appendChild(li);
+
+    const todoObj = {
+        text: text
+    }
+    toDos.push(todoObj);
+    saveToDo();
+}
+
+function submitToDo(event) {
+    event.preventDefault();
+    currentValue = todoInput.value;
+    // console.log(currentValue);
+    writeToDo(currentValue);
+    todoInput.value = "";
+}
 
 function loadToDo() {
-    const loadedToDo = localStorage.getItem(toDos);
-    if(loadedToDo !== null) {
-        
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach( function(toDo) {
+            writeToDo(toDo.text);
+        })
     }
 }
 
 function init() {
     loadToDo();
+    todoForm.addEventListener("submit", submitToDo);
 }
 init();
