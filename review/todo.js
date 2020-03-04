f9e8c2c3c782f395ca58f15a865507bb020da817
howdy-mj <hi.minjungkim@gmail.com>
2020-03-05 00:02:06 +0900

    4.3 Add delete ToDo

--- review/todo.js
+++ review/todo.js
@@ -5,6 +5,18 @@ const todoList = document.querySelector(".todoList");
 const TODOS_LS = "toDos";
 let toDos = [];
 
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    todoList.removeChild(li);
+
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id); // li.id가 String이라 숫자로 변환
+    });
+    toDos = cleanToDos;
+    saveToDo();
+}
+
 function saveToDo() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); // JS값이나 객체를 JSON 문자열로 변경
 }
@@ -14,16 +26,20 @@ function writeToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
 
     delBtn.innerText = "삭제";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
 
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
     todoList.appendChild(li);
 
     const todoObj = {
-        text: text
+        text: text,
+        id: newId
     }
     toDos.push(todoObj);
     saveToDo();
