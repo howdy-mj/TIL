aca2c915b5e59033918a18a96e1d52ab8619fe73
howdy-mj <hi.minjungkim@gmail.com>
2020-03-07 16:03:53 +0900

    6.1 Getting the weather, use API_KEY

--- review/weather.js
+++ review/weather.js
@@ -1,6 +1,22 @@
 const weather = document.querySelector(".weather");
 
+const API_KEY = "101c6d7169550352b0fd809f512fffd6";
 const COORDS = "coords";
+
+function getWeather(lat, lon) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric` //metric = 섭씨
+    ).then( function(respone) {
+        // fetch가 다 작동한 다음에 실행
+        return respone.json();
+    }).then( function(json) {
+        // response 불러온 후, object 불러오기
+        // console.log(json); // 잘 불려오는지 확인
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @${place}`;
+    })
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -19,6 +35,7 @@ function handleGeoSuccess(position) {
         longitude: longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function askForCoords() {
@@ -31,7 +48,9 @@ function loadCoords() {
     if (loadedCoords === null) {
         askForCoords();
     } else {
-        // getweather
+        const parseCoords = JSON.parse(loadedCoords);
+        // console.log(parseCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
